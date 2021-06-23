5ad02e16cf1433e21b8560b3b94734e723ec480b
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-06-23 12:34:09 +0200

    exchange logic of the game

--- script.js
+++ script.js
@@ -1,60 +1,80 @@
-/*get computer choice*/
-function computerPlay(){
-    let randomValues=['ROCK','PAPER','SCISSOR'];
-    return randomValues[Math.floor(Math.random()*randomValues.length)];
-}
-
-/*variables for tracking computer&player scores*/
-let computerWins = 0;
-let playerWins = 0;
-let playDraws = 0;
-
 const playerChoice = document.querySelector('#playerchoice');
 const computerChoice = document.querySelector('#computerchoice')
 const result = document.querySelector('#result');
 const playerScores = document.querySelector('#playerscores');
 const computerScores = document.querySelector('#computerscores');
 
+/*variables for tracking computer&player scores*/
+let computerWins = 0;
+let playerWins = 0;
+let playDraws = 0;
+
 /*variables for computer&player choices */
-const computerSelection = computerPlay();
+let computerSelection = '';
 let playerSelection = '';
+
+/*get computer choice*/
+function computerPlay(){
+  computerSelection = Math.floor(Math.random() * Math.floor(3));
+
+  switch (computerSelection) {
+  case 0:
+    return computerSelection = 'ROCK';
+    break;
+  case 1:
+    return computerSelection = 'PAPER';
+    break;
+  case 2:
+    return computerSelection = 'SCISSORS';
+    break;
+  }
+};
+
 
 /*starting game by clicking buttons*/
 const weapons = document.getElementsByClassName('weapon');
 
-
 Array.from(weapons).forEach((weapon)=>{
+
   weapon.addEventListener('click',(e)=>{
     playerSelection = weapon.id; playRound(computerSelection,playerSelection);   
-    playerChoice.textContent= " "+playerSelection;
-    computerChoice.textContent=" "+computerSelection;
     });
+  
 });
 
-
 /*single-round play function*/
-function playRound(computerSelection,playerSelection){
+ function playRound(computerSelection,playerSelection){
+
+  computerPlay();            
       if(
         (computerSelection==='ROCK'&& playerSelection==='SCISSOR')||
         (computerSelection==='SCISSOR'&& playerSelection==='PAPER')||
         (computerSelection==='PAPER'&& playerSelection==='ROCK')
-      ) {computerWins++; computerScores.textContent="Computer: "+computerWins;
-         result.textContent="You lose! Try again "}
+      ) {computerWins++;
+        
+        }
+         
       else if(
         (computerSelection==='ROCK'&& playerSelection==='PAPER')||
         (computerSelection==='PAPER'&& playerSelection==='SCISSOR')||
         (computerSelection==='SCISSOR'&& playerSelection==='ROCK')
-      ) {playerWins++; playerScores.textContent="Player: "+playerWins;
-        result.textContent="You win! Gratulations!"}
-      else if (
-        (computerSelection==='ROCK'&& playerSelection==='ROCK')||
-        (computerSelection==='PAPER'&& playerSelection==='PAPER')||
-        (computerSelection==='SCISSOR'&& playerSelection==='SCISSOR')
-      ){playDraws++; result.textContent= "It's draw"}
-      else {result.textContent="Check your choice please"};
-}
+      ) {playerWins++;
+        } 
+         
+      else if(computerSelection===playerSelection){
+         playDraws++;
+         };
 
+       playerScores.textContent= "player "+playerWins;
+       computerScores.textContent= "comp " +computerWins;
+       computerChoice.textContent= computerPlay(); 
+       playerChoice.textContent= playerSelection;
+       
+      
+  }
+      
 
+  
 
 /*multi-rounds play game and define of the winner
 const playGame=()=>{
@@ -86,4 +106,3 @@ playGame();
 
 
   
-
